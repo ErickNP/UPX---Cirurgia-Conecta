18afa57da570caf6aef63ff7e281f3d9fcca64b9
ErickNP <99359558+ErickNP@users.noreply.github.com>
2023-11-15 16:37:25 -0300

    Add files via upload
    Signed-off-by: ErickNP <99359558+ErickNP@users.noreply.github.com>

--- FornecedorService/src/jdbc/dao/MedicoDao.java
+++ FornecedorService/src/jdbc/dao/MedicoDao.java
@@ -1,5 +1,6 @@
 package jdbc.dao;
 
+import java.beans.Statement;
 import java.util.ArrayList;
 import jdbc.model.Medico;
 import java.sql.Connection;
@@ -41,17 +42,105 @@ public class MedicoDao implements IdaoInterface {
 
     @Override
     public Medico consultarM(int ID) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        String sql = "SELECT * FROM Medicos WHERE ID = ?;";
+        PreparedStatement comando;
+        ResultSet resultado;
+        try {
+            comando = conn.prepareStatement(sql);
+            comando.setInt(1, ID);
+            resultado = comando.executeQuery();
+
+            if (resultado.next()) {
+                Medico m = new Medico();
+                m.setId(resultado.getInt("ID"));
+                m.setNome(resultado.getString("Nome"));
+                m.setNomeUsuario(resultado.getString("NomeUsuario"));
+                m.setSenha(resultado.getString("Senha"));
+                return m;
+            } else {
+                return null;
+            }
+        } catch (SQLException err) {
+            throw new RuntimeException(err.getMessage());
+        }
+
     }
 
     @Override
     public Medico cadastrarM(Medico med) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        String sql = "INSERT INTO Medicos (Nome, NomeUsuario, Senha) VALUES (?, ?, ?);";
+        PreparedStatement comando;
+
+        try {
+            comando = conn.prepareStatement(sql);
+            comando.setString(1, med.getNome());
+            comando.setString(2, med.getNomeUsuario());
+            comando.setString(3, med.getSenha());
+
+            int linhasAfetadas = comando.executeUpdate();
+
+            if (linhasAfetadas == 0) {
+                throw new SQLException("Falha ao cadastrar o médico.");
+            }
+
+            return med;
+        } catch (SQLException err) {
+            throw new RuntimeException(err.getMessage());
+        }
+
     }
 
     @Override
     public boolean excluirM(int ID) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        String sql = "DELETE FROM Medicos WHERE ID = ?;";
+        PreparedStatement comando;
+
+        try {
+            comando = conn.prepareStatement(sql);
+            comando.setInt(1, ID);
+
+            int linhasAfetadas = comando.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException err) {
+            throw new RuntimeException(err.getMessage());
+        }
+    }
+
+    @Override
+    public Medico consultarM(String nomeUsuario, String senha) {
+
+        String sql = "SELECT * FROM Medicos WHERE NomeUsuario = ? AND Senha = ?;";
+        PreparedStatement comando;
+        ResultSet resultado;
+
+        try {
+            comando = conn.prepareStatement(sql);
+            comando.setString(1, nomeUsuario);
+            comando.setString(2, senha);
+
+            resultado = comando.executeQuery();
+
+            if (resultado.next()) {
+                Medico m = new Medico();
+                m.setId(resultado.getInt("ID"));
+                m.setNome(resultado.getString("Nome"));
+                m.setNomeUsuario(resultado.getString("NomeUsuario"));
+                m.setSenha(resultado.getString("Senha"));
+                return m;
+            } else {
+                return null;
+            }
+        } catch (SQLException err) {
+            throw new RuntimeException(err.getMessage());
+        }
     }
 
     //Herdados devido a declaração de interface
--- FornecedorService/src/jdbc/dao/PacienteDao.java
+++ FornecedorService/src/jdbc/dao/PacienteDao.java
@@ -19,7 +19,7 @@ public class PacienteDao implements IdaoInterface {
     @Override
     public ArrayList<Paciente> consultarP() {
         ArrayList<Paciente> lstPacientes = new ArrayList<>();
-        String sql = "SELECT * FROM Pacientes;";
+        String sql = "SELECT * FROM Medicos;";
         PreparedStatement comando;
         ResultSet resultado;
         try {
@@ -48,17 +48,83 @@ public class PacienteDao implements IdaoInterface {
 
     @Override
     public Paciente consultarP(int ID) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "SELECT * FROM Pacientes WHERE ID = ?;";
+        PreparedStatement comando;
+        ResultSet resultado;
+        try {
+            comando = conn.prepareStatement(sql);
+            comando.setInt(1, ID);
+            resultado = comando.executeQuery();
+
+            if (resultado.next()) {
+                Paciente p = new Paciente();
+                p.setId(resultado.getInt("ID"));
+                p.setNome(resultado.getString("Nome"));
+                p.setRg(resultado.getString("RG"));
+                p.setData(resultado.getDate("DataNasc"));
+                p.setConvenio(resultado.getString("Convenio"));
+                p.setNumeroConv(resultado.getInt("NumeroConv"));
+                p.setTelefone(resultado.getInt("Telefone"));
+                p.setEmail(resultado.getString("Email"));
+                p.setCidade(resultado.getString("Cidade"));
+                p.setCep(resultado.getString("CEP"));
+                return p;
+            } else {
+                return null;
+            }
+        } catch (SQLException err) {
+            throw new RuntimeException(err.getMessage());
+        }
     }
 
     @Override
     public Paciente cadastrarP(Paciente pac) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "INSERT INTO Pacientes (Nome, RG, DataNasc, Convenio, NumeroConv, Telefone, Email, Cidade, CEP) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+        PreparedStatement comando;
+
+        try {
+            comando = conn.prepareStatement(sql);
+            comando.setString(1, pac.getNome());
+            comando.setString(2, pac.getRg());
+            comando.setDate(3, pac.getData());
+            comando.setString(4, pac.getConvenio());
+            comando.setInt(5, pac.getNumeroConv());
+            comando.setInt(6, pac.getTelefone());
+            comando.setString(7, pac.getEmail());
+            comando.setString(8, pac.getCidade());
+            comando.setString(9, pac.getCep());
+            int linhasAfetadas = comando.executeUpdate();
+
+            if (linhasAfetadas == 0) {
+                throw new SQLException("Falha ao cadastrar o Paciente.");
+            }
+
+            return pac;
+        } catch (SQLException err) {
+            throw new RuntimeException(err.getMessage());
+        }
     }
 
     @Override
     public boolean excluirP(int ID) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        String sql = "DELETE FROM Pacientes WHERE ID = ?;";
+        PreparedStatement comando;
+
+        try {
+            comando = conn.prepareStatement(sql);
+            comando.setInt(1, ID);
+
+            int linhasAfetadas = comando.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException err) {
+            throw new RuntimeException(err.getMessage());
+        }
     }
 
     //Herdados devido a declaração de interface
@@ -79,6 +145,11 @@ public class PacienteDao implements IdaoInterface {
 
     @Override
     public boolean excluirM(int ID) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Medico consultarM(String nomeUsuario, String senha) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
